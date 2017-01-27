245b1623a86cf37672e9b20b1e5d248498decb07
lukasgartmair <lukas_gartmair@gmx.de>
2017-01-27 09:09:50 +0100

    changed plotting style

--- calc_triangle_faces.py
+++ calc_triangle_faces.py
@@ -10,12 +10,12 @@ import matplotlib.pyplot as pl
 from scipy.stats import norm
 import matplotlib.mlab as mlab
 
-vdb_verts = np.genfromtxt('sharp_vdb_vertices.txt')
-vdb_faces = np.genfromtxt('sharp_vdb_faces.txt',dtype=np.int64)
+vdb_verts = np.genfromtxt('erboco_vdb_vertices.txt')
+vdb_faces = np.genfromtxt('erboco_vdb_faces.txt',dtype=np.int64)
 vdb_faces = vdb_faces - 1
 
-ivas_verts = np.genfromtxt('sharp_ivas_vertices.txt')
-ivas_faces = np.genfromtxt('sharp_ivas_faces.txt',dtype=np.int64)
+ivas_verts = np.genfromtxt('erboco_ivas_vertices.txt')
+ivas_faces = np.genfromtxt('erboco_ivas_faces.txt',dtype=np.int64)
 ivas_faces = ivas_faces - 1
 
 p12 = np.zeros(3)
@@ -58,18 +58,47 @@ for i,f in enumerate(ivas_faces):
     
 bins = 50
 
-pl.hist(vdb_triangle_areas, bins=bins, label='DMC', color='gray')
+fig, ax = pl.subplots()
+
+ax.hist(vdb_triangle_areas, bins=bins, label='DMC', color='gray')
 pl.grid()
-pl.xlabel('triangle area / $nm^2$')
-pl.ylabel('frequency')
-pl.ylim(0,65)
+pl.tick_params(axis='both', which='major', labelsize=19)
+pl.yticks(fontname = "Times New Roman")  # This argument will change the font.
+pl.xticks(fontname = "Times New Roman")  # This argument will change the font.
+pl.rcParams['legend.fontsize'] = 18.0
+pl.xlabel('triangle area / $nm^2$',fontsize=22, fontname = "Times New Roman")
+pl.ylabel('frequency',fontsize=22, fontname = "Times New Roman")
+pl.ylim(0,45)
 pl.xlim(0,5)
 pl.legend()
+fig.tight_layout()
+
+fig.savefig('/home/lukas/master_thesis_newdesign/figures/vdb_bins50_erboco_faces_1000dpi.eps', format='eps', dpi=1000)
+
+
 pl.figure()
+fig2, ax = pl.subplots()
+
 pl.hist(ivas_triangle_areas, bins=bins,label='MC', color='blue')
 pl.grid()
+pl.tick_params(axis='both', which='major', labelsize=19)
+pl.yticks(fontname = "Times New Roman")  # This argument will change the font.
+pl.xticks(fontname = "Times New Roman")  # This argument will change the font.
+pl.rcParams['legend.fontsize'] = 18.0
 pl.xlim(0,5)
-pl.ylim(0,65)
-pl.xlabel('triangle area / $nm^2$')
-pl.ylabel('frequency')
+pl.ylim(0,45)
+pl.xlabel('triangle area / $nm^2$',fontsize=22, fontname = "Times New Roman")
+pl.ylabel('frequency',fontsize=22, fontname = "Times New Roman")
 pl.legend()
+fig2.tight_layout()
+
+fig2.savefig('/home/lukas/master_thesis_newdesign/figures/ivas_bins50_erboco_faces_1000dpi.eps', format='eps', dpi=1000)
+
+#pl.tick_params(axis='both', which='major', labelsize=19)
+#pl.yticks(fontname = "Times New Roman")  # This argument will change the font.
+#pl.xticks(fontname = "Times New Roman")  # This argument will change the font.
+#pl.xlabel('triangle index ' ,fontsize=22, fontname = "Times New Roman")
+#pl.ylabel('scalar triple product', fontsize=22, fontname = "Times New Roman")
+#pl.grid()
+#pl.rcParams['legend.fontsize'] = 18.0
+#pl.legend()
